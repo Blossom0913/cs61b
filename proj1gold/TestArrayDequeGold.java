ae050e04faaaab90e43397b719fc2dd425ec3abe
chenxutian <chenxutian@stu2021.jnu.edu.cn>
2025-09-19 19:45:02 +0800

    proj1goldfix2

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -10,38 +10,38 @@ public class TestArrayDequeGold {
     public void testStudentArrayDeque() {
         StudentArrayDeque<Integer> studentArrayDeque = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> arrayDequeSolution = new ArrayDequeSolution<>();
-        //ArrayList<String> callTrace = new ArrayList<String>(); // function call stack
-        for (int i = 0; i < 10; i += 1) {
+        String log = "";
+        for (int i = 0; i < 1000; i += 1) {
             double numberOne = StdRandom.uniform();
             double numberZero = StdRandom.uniform();
             Integer x = i;
             if (numberZero < 0.5 && numberOne < 0.5) {
                 studentArrayDeque.addFirst(x);
                 arrayDequeSolution.addFirst(x);
-                assertEquals("addFirst faild,student's: " + studentArrayDeque.get(0) +
-                        " not equal to" + arrayDequeSolution.get(0) + "!\n",studentArrayDeque.get(0),arrayDequeSolution.get(0));
+                log = log + "addFirst(" + x + ")\n";
+                assertEquals(log,studentArrayDeque.get(0),arrayDequeSolution.get(0));
                 //callTrace.addLast("addFirst");
             }
             if (numberZero >= 0.5 && numberOne < 0.5) {
                 studentArrayDeque.addLast(x);
                 arrayDequeSolution.addLast(x);
-                assertEquals("addLast faild,student's: " + studentArrayDeque.get(studentArrayDeque.size() - 1) +
-                        " not equal to" + arrayDequeSolution.get(arrayDequeSolution.size() - 1) + "!\n",studentArrayDeque.get(studentArrayDeque.size() - 1),arrayDequeSolution.get(arrayDequeSolution.size() - 1));
+                log = log + "addLast(" + x + ")\n";
+                assertEquals(log,studentArrayDeque.get(studentArrayDeque.size() - 1),arrayDequeSolution.get(arrayDequeSolution.size() - 1));
                 //callTrace.addLast("addLast");
             }
             if (numberZero < 0.5 && numberOne >= 0.5) {
                 if (studentArrayDeque.isEmpty() || arrayDequeSolution.isEmpty()){continue;}
                 Integer student = studentArrayDeque.removeFirst();
                 Integer array = arrayDequeSolution.removeFirst();
-                assertEquals("removeFirst failed, student's: " + student + "not equal to: " + array + "!\n",student,array);
-                //callTrace.addLast("removeFirst");
+                log = log + "removeFirst()\n";
+                assertEquals(log,student,array);
             }
             if (numberZero >= 0.5 && numberOne >= 0.5) {
                 if (studentArrayDeque.isEmpty() || arrayDequeSolution.isEmpty()){continue;}
                 Integer student = studentArrayDeque.removeLast();
                 Integer array = arrayDequeSolution.removeLast();
-                assertEquals("removeLast failed, student's: " + student + "not equal to: " + array + "!\n",student,array);
-                //callTrace.addLast("removeLast");
+                log = log + "removeLast()\n";
+                assertEquals(log,student,array);
             }
 
         }
